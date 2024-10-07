b5648098621c6a8154d5beb5852158b7e6797a04
Blueion <128919662+Blueion76@users.noreply.github.com>
2024-10-07 12:36:29 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ CONFIG_PATH=/data/options.json
 EUFY_WS_PORT=${EUFY_SECURITY_WS_PORT:-3000}
 
 echo "Starting EufyP2PStream. eufy_security_ws_port is $EUFY_WS_PORT"
-python3 -u /eufyp2pstream.py $EUFY_WS_PORT
+python3 -u /app/eufyp2pstream.py $EUFY_WS_PORT
 echo "Exited with code $?"
