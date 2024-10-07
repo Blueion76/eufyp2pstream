c684f2954631822dc37d2b72562ae2df44f9f925
Blueion <128919662+Blueion76@users.noreply.github.com>
2024-10-07 12:24:58 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set +u
+CONFIG_PATH=/data/options.json
+
+# Read the port from the environment variable or default to 3000
+EUFY_WS_PORT=${EUFY_SECURITY_WS_PORT:-3000}
+
+echo "Starting EufyP2PStream. eufy_security_ws_port is $EUFY_WS_PORT"
+python3 -u /eufyp2pstream.py $EUFY_WS_PORT
+echo "Exited with code $?"
